677b2116bdb7efe9cfbe23eac37782d0f4a669ae
Tomasz <tmster@Mac-mini-Tomasz.local>
2013-07-17 20:51:27 +0200

    rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Forelli::Application.config.secret_token = '4f0bdb23a272eeca30747693219f19a8b4e716191bc03c08017476daa0606f88f2000f33a678a85481084b118fced46cd5c95821fffdb87b34fa47fb4859e0e1'
